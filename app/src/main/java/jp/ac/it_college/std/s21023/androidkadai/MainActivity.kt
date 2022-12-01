e9dbb36a5663485417d2d60f7291e99becba41cc
s21023 <s21023@std.it-college.ac.jp>
2022-12-01 11:35:39 +0900

    MainActivity.kt編集

--- app/src/main/java/jp/ac/it_college/std/s21023/androidkadai/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s21023/androidkadai/MainActivity.kt
@@ -6,25 +6,13 @@ import jp.ac.it_college.std.s21023.androidkadai.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    //    private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
-//    private var pokemonList: List<Poke>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //initPokemonList()
-
-
-        //画面遷移をわかりやすくするため
 
     }
-//    private fun initPokemonList() {
-//        val jsonStr = resources.assets.open("ordered_pokemon.json").reader().readText()
-//        val type = Types.newParameterizedType(List::class.java, Poke::class.java)
-//        val adapter: JsonAdapter<List<Poke>> = moshi.adapter(type)
-//
-//        pokemonList = adapter.fromJson(jsonStr)
-//
-//    }
+
 }
