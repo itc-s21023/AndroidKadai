adf885e91413b3a92540ca69d3cc917f2d8dd997
s21023 <s21023@std.it-college.ac.jp>
2022-12-01 10:38:30 +0900

    MainActivity.kt編集

--- app/src/main/java/jp/ac/it_college/std/s21023/androidkadai/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s21023/androidkadai/MainActivity.kt
@@ -2,10 +2,43 @@ package jp.ac.it_college.std.s21023.androidkadai
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import jp.ac.it_college.std.s21023.pokemonquiz.databinding.ActivityMainBinding
+import jp.ac.it_college.std.s21023.pokemonquiz.service.Poke
+import jp.ac.it_college.std.s21023.androidkadai.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    //    private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+//    private var pokemonList: List<Poke>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //initPokemonList()
+
+
+        //画面遷移をわかりやすくするため
+        val navController =
+            binding.fragmentContainerView.getFragment<NavHostFragment>().navController
+        binding.toolbar.setupWithNavController(
+            navController,
+            AppBarConfiguration(navController.graph)
+        )
     }
+//    private fun initPokemonList() {
+//        val jsonStr = resources.assets.open("ordered_pokemon.json").reader().readText()
+//        val type = Types.newParameterizedType(List::class.java, Poke::class.java)
+//        val adapter: JsonAdapter<List<Poke>> = moshi.adapter(type)
+//
+//        pokemonList = adapter.fromJson(jsonStr)
+//
+//    }
 }
