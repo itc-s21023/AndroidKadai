44b403ab91db1b64820aa137ec60f06fa10af243
s21023 <s21023@std.it-college.ac.jp>
2022-12-02 16:04:57 +0900

    Service編集

--- app/src/main/java/jp/ac/it_college/std/s21023/androidkadai/service/PokemonService.kt
+++ app/src/main/java/jp/ac/it_college/std/s21023/androidkadai/service/PokemonService.kt
@@ -1,20 +1,13 @@
-package jp.ac.it_college.std.s21023.androidkadai.service
+package jp.ac.it_college.std.s21023.androidkadai
 
-import retrofit2.Response
+import jp.ac.it_college.std.s21023.androidkadai.json.PokemonInfo
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface PokemonService {
-    @GET("https://pokeapi.co/api/v2/")
-    suspend fun getPokemonInfo(
-        @Path("pokeId") pokeId: Int
-    ): Response<PokemonResponse>
+    @GET("https://pokeapi.co/api/v2/{id}")
+    fun getPokemon(
+        @Path("id")id: Int
+    ): Call<PokemonInfo>
 }
-data class PokemonResponse(
-    val pokemon: List<Poke>
-)
-
-data class Poke(
-    val id: Int,
-    val name: String
-)
